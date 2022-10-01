43df6c30744877d9aa1848e06f2e6eb14fe33a97
RatniChaima <ratnichayma@gmail.com>
2022-10-01 13:55:05 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//const cors = require("cors");
+//const compression = require("compression");
+const app = express()
+//const app = express().use("*", cors());
+// app.use(compression());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// //DB Connection
+// require("./src/database/db").connection();
+
+// //Routes
+// const routes = require("./src/routes/index");
+// app.use("/", routes);
+
+app.get('/', function(req, res) {
+  res.send('Todo list backend v1')
+})
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000 ...");
+});
